0878221ace30b3c88e280d25223fea15272ead4e
Your Name <you@example.com>
2024-11-27 21:16:29 +0000

    Implement full list of papersizes per printer

--- src/get-printers/get-printers.ts
+++ src/get-printers/get-printers.ts
@@ -4,29 +4,26 @@ import throwIfUnsupportedOperatingSystem from "../utils/throw-if-unsupported-os"
 import { Printer } from "..";
 
 async function getPrinters(): Promise<Printer[]> {
-  function stdoutHandler(stdout: string) {
-    const printers: Printer[] = [];
 
-    stdout
-      .split(/(\r?\n){2,}/)
-      .map((printer) => printer.trim())
-      .filter((printer) => !!printer)
-      .forEach((printer) => {
-        const { isValid, printerData } = isValidPrinter(printer);
+  function stdoutHandler(stdout: string): Printer[] {
+    const parsedPrinters: {
+      Name: string;
+      DeviceID: string;
+      PrinterPaperNames: string;
+    }[] = JSON.parse(stdout);
 
-        if (!isValid) return;
-
-        printers.push(printerData);
-      });
-
-    return printers;
+    return parsedPrinters.map((printer) => ({
+      name: printer.Name,
+      deviceId: printer.DeviceID,
+      paperSizes: printer.PrinterPaperNames.split(", ").map((name: string) => name.trim()),
+    }));
   }
 
   try {
     throwIfUnsupportedOperatingSystem();
     const { stdout } = await execFileAsync("Powershell.exe", [
       "-Command",
-      `Get-CimInstance Win32_Printer -Property DeviceID,Name,PrinterPaperNames`,
+      `Get-CimInstance Win32_Printer -Property DeviceID,Name,PrinterPaperNames | ForEach-Object { $_ | Add-Member -MemberType NoteProperty -Name PrinterPaperNames -Value ($_.PrinterPaperNames -join ', ') -Force; $_ } | Select-Object Name, DeviceID, PrinterPaperNames | ConvertTo-Json -Depth 2`,
     ]);
     return stdoutHandler(stdout);
   } catch (error) {
